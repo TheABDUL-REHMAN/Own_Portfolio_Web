cde4cd301a09560fd20c6edfee4802c1268f9f37
ABDULREHMAN <abdulrehman03044642422@gmail.com>
2025-09-20 22:45:00 -0700

    Main

--- src/app/Components/Navbar/NavbarMain.js
+++ src/app/Components/Navbar/NavbarMain.js
@@ -1,4 +1,3 @@
-
 'use client';
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
@@ -38,7 +37,7 @@ const NavbarMain = () => {
         transition={{ duration: 0.5 }}
         className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${isScrolled ? 'py-2 bg-gray-900/80 backdrop-blur-md border-b border-purple-900/20' : 'py-4 bg-transparent'}`}
       >
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="w-full px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
             <Link href="/" className="flex-shrink-0">
@@ -46,7 +45,6 @@ const NavbarMain = () => {
                 whileHover={{ scale: 1.05 }}
                 className="flex items-center"
               >
-                {/* Add image here */}
                 <Image
                   src="/PM.png"
                   alt="Profile"
